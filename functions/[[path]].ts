52f14804222f287dc3268610cd3f342d80f11b92
tenzonp <novacompanynp@gmail.com>
2026-06-06 12:53:21 +0545

    Implement onRequest function to fetch instance

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,1 +1,8 @@
+import { startInstance } from "../src/start";
 
+export async function onRequest(context: any) {
+  const { request, env, ctx } = context;
+
+  const response = await startInstance.fetch(request, env, ctx);
+  return response;
+}
