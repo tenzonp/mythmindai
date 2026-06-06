f0aea98c51eff71e4949d21369a0fe68f0f52b10
tenzonp <novacompanynp@gmail.com>
2026-06-06 13:10:52 +0545

    Delete functions directory

--- functions/[[path]].ts
+++
@@ -1,8 +0,0 @@
-import { startInstance } from "../src/start";
-
-export async function onRequest(context: any) {
-  const { request, env, ctx } = context;
-
-  const response = await startInstance.fetch(request, env, ctx);
-  return response;
-}
